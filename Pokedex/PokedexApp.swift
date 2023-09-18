0dd87ad8a703ce570b7ae35234e90317084e5c5a
Roberto Evangelista <robertoevangelistasf@gmail.com>
2023-09-18 22:58:24 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Roberto Evangelista on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
