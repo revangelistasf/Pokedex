3bff87af8f2db081002a19ba9faac117e8fcf556
Roberto Evangelista <robertoevangelistasf@gmail.com>
2023-09-28 21:44:01 +0100

    feat: create PokemonPagedResult

--- Pokedex/Domain/Entities/Pokemon.swift
+++ Pokedex/Domain/Entities/Pokemon.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
-struct Pokemon {
+struct Pokemon: Decodable {
     let id: String
     let name: String
+}
+
+struct PokemonPagedResult: Decodable {
+    let count: Int
+    let next: String?
+    let previous: String?
+    let results: [PokemonResult]
+}
+
+struct PokemonResult: Decodable {
+    let name: URL
+    let url: String
 }
---
+++ Pokedex/Infrastructure/Network/NetworkError.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkError.swift
+//  Pokedex
+//
+//  Created by Roberto Evangelista on 27/09/2023.
+//
+
+import Foundation
